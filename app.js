3298cc092953d420f93d62bcf76672fdbe92cc06
Kemal Ersin Yılmaz <mail@kemalersin.com>
2016-06-09 14:48:00 +0300

    Ignore MongoDB connection.

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ var express = require('express'),
   glob = require('glob'),
   mongoose = require('mongoose');
 
-mongoose.connect(config.db);
-var db = mongoose.connection;
-db.on('error', function () {
-  throw new Error('unable to connect to database at ' + config.db);
-});
+//mongoose.connect(config.db);
+//var db = mongoose.connection;
 
-var models = glob.sync(config.root + '/app/models/*.js');
-models.forEach(function (model) {
-  require(model);
-});
+// db.on('error', function () {
+//   throw new Error('unable to connect to database at ' + config.db);
+// });
+
+// var models = glob.sync(config.root + '/app/models/*.js');
+//
+// models.forEach(function (model) {
+//   require(model);
+// });
+
 var app = express();
 
 require('./config/express')(app, config);
