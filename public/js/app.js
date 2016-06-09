afc084d5db9ab1432cd8094bbc873efd857d5e41
kemalersin <mail@kemalersin.com>
2016-06-10 00:05:49 +0300

    Add scroll effect.

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,54 @@
-(function () {
+(function ($) {
+  'use strict';
+
   function updateHeatmap(event) {
     var map = event.chart;
+
     if (map.dataGenerated)
       return;
+
     if (map.dataProvider.areas.length === 0) {
       setTimeout(updateHeatmap, 100);
       return;
     }
+
     for (var i = 0; i < map.dataProvider.areas.length; i++) {
       map.dataProvider.areas[i].value = Math.round(Math.random() * 10000);
     }
+
     map.dataGenerated = true;
     map.validateNow();
+
+    $('.amcharts-chart-div a').remove();
   }
+
+  $('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: 51
+  });
+
+  window.sr = ScrollReveal();
+
+  sr.reveal('.sr-contact', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 300);
+
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+
+    event.preventDefault();
+  });
+
+  $('.navbar-fixed-top').autoHidingNavbar({
+    hideOffset: 200,
+    showOnBottom: false
+  });
 
   AmCharts.makeChart("map", {
     "type": "map",
@@ -39,4 +75,4 @@
 
     "listeners": [{ "event": "init", "method": updateHeatmap }]
   });
-})();
+})(jQuery);
