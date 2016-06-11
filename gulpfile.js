5222b1ebad6f2cd5c60d9bb5871792bff01fed0a
kemalersin <mail@kemalersin.com>
2016-06-12 01:30:23 +0300

    Add minify task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp'),
   nodemon = require('gulp-nodemon'),
   plumber = require('gulp-plumber'),
   livereload = require('gulp-livereload'),
+  minify = require('gulp-minify'),
+  cleanCSS = require('gulp-clean-css'),
   sass = require('gulp-sass');
 
 gulp.task('sass', function () {
@@ -33,8 +35,30 @@ gulp.task('develop', function () {
   });
 });
 
+gulp.task('compress', function() {
+  gulp.src('./public/js/*.js')
+    .pipe(minify({
+      ext: {
+        src: '.js',
+        min: '.min.js'
+      },
+      exclude: ['tasks'],
+      ignoreFiles: ['*min.js']
+    }))
+    .pipe(gulp.dest('./public/js'))
+    .pipe(livereload());
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('public/css/*.css')
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('public/css/'));
+});
+
 gulp.task('default', [
   'sass',
   'develop',
+  'compress',
+  'minify-css',
   'watch'
 ]);
