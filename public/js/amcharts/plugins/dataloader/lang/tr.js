14f162d01135217d922740acf6400f374cc0dc45
kemalersin <mail@kemalersin.com>
2016-06-13 00:03:55 +0300

    Remove .idea

---
+++ public/js/amcharts/plugins/dataloader/lang/tr.js
@@ -0,0 +1,6 @@
+AmCharts.translations.dataLoader.tr = {
+  'Error loading the file': 'Dosya yüklenirken hata oluştu',
+  'Error parsing JSON file': 'JSON dosyası ayrıştırılamadı',
+  'Unsupported data format': 'Desteklenmeye veri biçimi',
+  'Loading data...': 'Veri yükleniyor...'
+}
